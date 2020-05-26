6fddf5c7723f8466efccf43be3af97ae8895e639
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-26 17:49:16 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const PORT = 3333;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Docker!');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log('Server is running on port 3333');
+});
